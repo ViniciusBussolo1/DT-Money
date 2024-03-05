1e9bc215d53e9053dc4d0b2b1a313800a356035e
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-03-05 10:17:56 -0300

    :sparkles: feat: create component CardItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { CardItem } from '@/components/card-item/card-item'
 import { Header } from '@/components/header/header'
 
 export default function Home() {
   return (
-    <main className="max-w-[70rem] w-full pt-12">
+    <main className="max-w-[70rem] w-full pt-12 px-2">
       <Header />
+      <div className="w-full flex gap-6 mt-10">
+        <CardItem title="Entradas" transacao="entrada" preco={17400} />
+        <CardItem title="Saidas" transacao="saida" preco={1259} />
+        <CardItem title="Total" preco={16141} />
+      </div>
     </main>
   )
 }
---
+++ src/components/card-item/card-item.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+
+import IconEntrada from '../../../public/incon-entrada.svg'
+import IconSaida from '../../../public/icon-saida.svg'
+
+import { DollarSign } from 'lucide-react'
+
+interface CardItemProps {
+  title: 'Entradas' | 'Saidas' | 'Total'
+  transacao?: 'entrada' | 'saida'
+  preco: number
+}
+
+export function CardItem({ title, transacao, preco }: CardItemProps) {
+  return (
+    <Card className="max-w-[22rem] w-full">
+      <CardHeader className="w-full flex flex-row items-center justify-between">
+        <CardTitle className="text-base font-semibold">{title}</CardTitle>
+        {transacao === 'entrada' ? (
+          <Image src={IconEntrada} alt="Icone de entrada" />
+        ) : transacao === 'saida' ? (
+          <Image src={IconSaida} alt="Icone de saida" />
+        ) : (
+          <DollarSign />
+        )}
+      </CardHeader>
+      <CardContent>
+        <span className="text-3xl font-bold">
+          {preco.toLocaleString('pt-BR', {
+            currency: 'BRL',
+            style: 'currency',
+          })}
+        </span>
+      </CardContent>
+    </Card>
+  )
+}
